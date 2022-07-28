c77ff3d7c96169bd5f22a252fc9da215044413e7
Caio Mori <caiomori22@gmail.com>
2022-07-28 11:46:12 -0300

    fix(navigation): fix changes

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,14 +1,16 @@
 import {createNativeStackNavigator} from '@react-navigation/native-stack'
 import Loading from '../components/Loading'
+import {LoginScreen} from '../screens/LoginScreen'
 
 const {Navigator, Screen} = createNativeStackNavigator()
 
 export function AppRoutes() {
   return (
     <Navigator screenOptions={{headerShown: false}}>
+      <Screen name='login' component={LoginScreen} />
       <Screen name='home' component={Loading} />
-      <Screen name='register' component={Loading} />
-      <Screen name='details' component={Loading} />
+      <Screen name='registration' component={Loading} />
+      <Screen name='conclusion' component={Loading} />
     </Navigator>
   )
 }
